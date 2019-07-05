28137702b1edd4cfc3694541c1c991a881de21e2
manlix <manlix@yandex.ru>
2019-07-05 19:48:25 +0300

    Add both requirements and setup files for the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""Setup file to install 'truedoc' package and its depends."""
+
+import setuptools
+
+setuptools.setup(
+    name="truedoc",
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+    ]
+)
