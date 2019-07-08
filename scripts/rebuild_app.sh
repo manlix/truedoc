03ef58c3baeb61a57ea58326fdcee4ed85a5298e
manlix <manlix@yandex.ru>
2019-07-08 13:11:35 +0300

    [scripts] Add script 'rebuild_app.sh' to rebuild app with deps

---
+++ scripts/rebuild_app.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Rebuild truedoc-app with deps
+
+# shellcheck source=/dev/null
+. "$(dirname "$0")/common.sh"
+
+
+function main() {
+
+    caption "Rebuild truedoc-app with deps"
+
+    docker::installed || die "No Docker installed"
+    docker_compose::installed || die "No docker-compose installed"
+
+    cd "$(dirname "$0")/../" || die "Cannot open source dir"
+    docker-compose -f docker-compose.dev.yml build --no-cache || die "Failed start by docker-compose"
+}
+
+main
