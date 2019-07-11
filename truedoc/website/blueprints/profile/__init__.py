c6f1f885e461d52431de043d0dab768767a0f9e3
manlix <manlix@yandex.ru>
2019-07-11 23:24:30 +0300

    [Blueprints] Profile: add ability to list and create profile

--- truedoc/website/blueprints/profile/__init__.py
+++ truedoc/website/blueprints/profile/__init__.py
@@ -7,11 +7,15 @@ from flask import request
 from ....constants import HTTP_METHOD
 from ....constants import STATUS
 
+from ....db import db
+
 bp = Blueprint('profile', __name__)
 
 
+# Create profile
 @bp.route('/', methods=[HTTP_METHOD.POST])
 def create_profile():
+    """Create profile."""
 
     if not request.is_json:
         return jsonify(
@@ -20,9 +24,25 @@ def create_profile():
         )
 
     else:
-        return jsonify(
 
-            status=STATUS.SUCCESS
+        profile = db.models.Profile(
+            email=request.json['email'],
+        )
+
+        profile.set_password(request.json['password'])
+
+        db.Profile.create_profile(profile)
+
+        return jsonify(
+            status=STATUS.SUCCESS,
         )
 
 
+# List pr
+@bp.route('/', methods=[HTTP_METHOD.GET])
+def list_profiles():
+    """List profiles."""
+    return jsonify(
+        status=STATUS.SUCCESS,
+        users=db.Profile.list_all(),
+    )
