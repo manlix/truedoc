4d02340168e0f531dcc9193a42b33f85fb677d93
manlix <manlix@yandex.ru>
2019-07-11 21:37:33 +0300

    [Blueprints] Update response structure caused by error

--- truedoc/website/blueprints/error/__init__.py
+++ truedoc/website/blueprints/error/__init__.py
@@ -3,19 +3,30 @@ from http import HTTPStatus
 from flask import Blueprint
 from flask import jsonify
 
+from ...constants import STATUS
+
 bp = Blueprint('error', __name__)
 
 
 @bp.app_errorhandler(401)
 def error_handler_401(e):
-    return jsonify(error_code=HTTPStatus.UNAUTHORIZED)
+    return jsonify(
+        error_code=HTTPStatus.UNAUTHORIZED,
+        status=STATUS.ERROR,
+    )
 
 
 @bp.app_errorhandler(404)
 def error_handler_404(e):
-    return jsonify(error_code=HTTPStatus.NOT_FOUND)
+    return jsonify(
+        error_code=HTTPStatus.NOT_FOUND,
+        status=STATUS.ERROR,
+    )
 
 
 @bp.app_errorhandler(500)
 def error_handler_401(e):
-    return jsonify(error_code=HTTPStatus.INTERNAL_SERVER_ERROR)
+    return jsonify(
+        error_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+        status=STATUS.ERROR,
+    )
