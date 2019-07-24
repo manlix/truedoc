21e9fbdbd0b7c0f283412a65f7316eb4ce58f65b
manlix <manlix@yandex.ru>
2019-07-25 00:24:04 +0300

    Add TODO to 'response' module about skipping 'http_code' key from kwargs

--- truedoc/response.py
+++ truedoc/response.py
@@ -4,11 +4,13 @@ from flask import jsonify
 from .constants import STATUS
 
 
+# TODO: add skipping 'http_code' key from 'kwargs' because HTTP code already has this code
 def failure(**kwargs):
     return jsonify({'status': STATUS.ERROR, **kwargs}), \
            kwargs['http_code'] if 'http_code' in kwargs else HTTPStatus.BAD_REQUEST  # 400
 
 
+# TODO: add skipping 'http_code' key from 'kwargs' because HTTP code already has this code
 def success(**kwargs):
     return jsonify({'status': STATUS.SUCCESS}), \
            kwargs['http_code'] if 'http_code' in kwargs else HTTPStatus.OK  # 200
