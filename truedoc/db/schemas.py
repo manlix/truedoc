26215c30b74ba4bdbf12b065ce7003f522994db0
manlix <manlix@yandex.ru>
2019-07-25 00:11:20 +0300

    Update schema for 'create profile'

--- truedoc/db/schemas.py
+++ truedoc/db/schemas.py
@@ -5,18 +5,6 @@ from marshmallow import validate
 
 class ProfileSchema(Schema):
     id = fields.UUID()
-    email = fields.Email(
-        required=True,
-        validate=[
-            validate.Email(),
-            validate.Length(max=128),
-        ]
-    )
-    password = fields.String(
-        load_only=True,
-        required=True,
-        validate=[
-            validate.Length(min=8, max=128),
-        ],
-    )
+    email = fields.Email(required=True, validate=[validate.Length(max=128)])
+    password = fields.String(load_only=True, required=True, validate=[validate.Length(min=8, max=128)])
     created_at = fields.DateTime(dump_only=True)
