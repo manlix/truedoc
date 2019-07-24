06b7d55add6120ce9ac017d2ab7f5fc275f868e3
manlix <manlix@yandex.ru>
2019-07-25 00:06:28 +0300

    Update 'error' blueprint

--- truedoc/website/blueprints/error/__init__.py
+++ truedoc/website/blueprints/error/__init__.py
@@ -1,32 +1,14 @@
-from http import HTTPStatus
-
 from flask import Blueprint
-from flask import jsonify
-
-from ....constants import STATE
+from ....response import failure
 
 bp = Blueprint('error', __name__)
 
 
 @bp.app_errorhandler(401)
 def error_handler_401(e):
-    return jsonify(
-        error_code=HTTPStatus.UNAUTHORIZED,
-        status=STATE.ERROR,
-    )
+    return failure(http_code=401)
 
 
 @bp.app_errorhandler(404)
 def error_handler_404(e):
-    return jsonify(
-        error_code=HTTPStatus.NOT_FOUND,
-        status=STATE.ERROR,
-    )
-
-
-@bp.app_errorhandler(500)
-def error_handler_401(e):
-    return jsonify(
-        error_code=HTTPStatus.INTERNAL_SERVER_ERROR,
-        status=STATE.ERROR,
-    )
+    return failure(http_code=404)
