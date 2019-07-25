666740388851f3db5a4ec9926b101344cec6bd1e
manlix <manlix@yandex.ru>
2019-07-25 17:47:16 +0300

    Add handler for all unknown exceptions

--- truedoc/website/app.py
+++ truedoc/website/app.py
@@ -4,10 +4,18 @@ from .blueprints import error
 from .blueprints import profile
 
 from ..db import db
+from ..response import failure
 
 app = Flask(__name__)
 app.register_blueprint(error.bp)
 app.register_blueprint(profile.bp, url_prefix='/profile')
 
+
 # See Generic Exception Handlers:
 # https://flask.palletsprojects.com/en/1.1.x/errorhandling/
+
+# TODO: think about parse for 'e'
+@app.errorhandler(Exception)
+def handler_all_exceptions(e):
+    """Catch all unhandled exception."""
+    return failure(http_code=500)
