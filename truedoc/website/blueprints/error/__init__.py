b671281cd68fff05428772f6b29d901351694450
manlix <manlix@yandex.ru>
2019-07-27 11:37:46 +0300

    [Blueprint] Add some comments to 'error' blueprint

--- truedoc/website/blueprints/error/__init__.py
+++ truedoc/website/blueprints/error/__init__.py
@@ -1,21 +1,19 @@
-from http import HTTPStatus
-
 from flask import Blueprint
-from ....response import failure
+from truedoc.response import failure
 
 bp = Blueprint('error', __name__)
 
 
-@bp.app_errorhandler(401)
+@bp.app_errorhandler(401)  # UNAUTHORIZED
 def error_handler_401(e):
     return failure(http_code=401)
 
 
-@bp.app_errorhandler(404)
+@bp.app_errorhandler(404)  # NOT_FOUND
 def error_handler_404(e):
     return failure(http_code=404)
 
 
-@bp.app_errorhandler(405)
+@bp.app_errorhandler(405)  # METHOD_NOT_ALLOWED
 def error_handler_405(e):
     return failure(http_code=405)
