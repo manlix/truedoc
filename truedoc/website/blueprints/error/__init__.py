b4030a37404b7572deb1df24082439f2ab0df8f6
manlix <manlix@yandex.ru>
2019-07-27 19:58:48 +0300

    Prepare JSON response for requests finished with failed result

--- truedoc/website/blueprints/error/__init__.py
+++ truedoc/website/blueprints/error/__init__.py
@@ -1,19 +1,33 @@
+from http import HTTPStatus
+
 from flask import Blueprint
+
+import werkzeug.exceptions
+
 from truedoc.response import failure
 
 bp = Blueprint('error', __name__)
 
 
-@bp.app_errorhandler(401)  # UNAUTHORIZED
+@bp.app_errorhandler(HTTPStatus.UNAUTHORIZED)
 def error_handler_401(e):
-    return failure(http_code=401)
+    return failure(http_code=HTTPStatus.UNAUTHORIZED)
 
 
-@bp.app_errorhandler(404)  # NOT_FOUND
+@bp.app_errorhandler(HTTPStatus.NOT_FOUND)
 def error_handler_404(e):
-    return failure(http_code=404)
+    return failure(http_code=HTTPStatus.NOT_FOUND)
 
 
-@bp.app_errorhandler(405)  # METHOD_NOT_ALLOWED
+@bp.app_errorhandler(HTTPStatus.METHOD_NOT_ALLOWED)
 def error_handler_405(e):
-    return failure(http_code=405)
+    return failure(http_code=HTTPStatus.METHOD_NOT_ALLOWED)
+
+
+@bp.app_errorhandler(werkzeug.exceptions.BadRequest)
+def handle_bad_request(e):
+    """Catch incorrect JSON in requests with Content-Type = application/json"""
+    return failure(
+        http_code=werkzeug.exceptions.BadRequest.code,
+        description=werkzeug.exceptions.BadRequest.description,
+    )
