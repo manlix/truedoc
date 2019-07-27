7c51a26d5b9fb35442553a375555be903d8763b4
manlix <manlix@yandex.ru>
2019-07-28 00:35:36 +0300

    [Errors] Add descriptions to response

--- truedoc/website/blueprints/error/__init__.py
+++ truedoc/website/blueprints/error/__init__.py
@@ -11,17 +11,17 @@ bp = Blueprint('error', __name__)
 
 @bp.app_errorhandler(HTTPStatus.UNAUTHORIZED)
 def error_handler_401(e):
-    return failure(http_code=HTTPStatus.UNAUTHORIZED)
+    return failure(http_code=HTTPStatus.UNAUTHORIZED, description=HTTPStatus.UNAUTHORIZED.description)
 
 
 @bp.app_errorhandler(HTTPStatus.NOT_FOUND)
 def error_handler_404(e):
-    return failure(http_code=HTTPStatus.NOT_FOUND)
+    return failure(http_code=HTTPStatus.NOT_FOUND, description=HTTPStatus.NOT_FOUND.description)
 
 
 @bp.app_errorhandler(HTTPStatus.METHOD_NOT_ALLOWED)
 def error_handler_405(e):
-    return failure(http_code=HTTPStatus.METHOD_NOT_ALLOWED)
+    return failure(http_code=HTTPStatus.METHOD_NOT_ALLOWED, description=HTTPStatus.METHOD_NOT_ALLOWED.description)
 
 
 @bp.app_errorhandler(werkzeug.exceptions.BadRequest)
