4717aefb229764bca9a49303d05f2c27b24b5d82
manlix <manlix@yandex.ru>
2019-07-27 11:42:34 +0300

    Reject 'id' field for input in schema for profile (in dumping only)

--- truedoc/db/schemas.py
+++ truedoc/db/schemas.py
@@ -4,7 +4,7 @@ from marshmallow import validate
 
 
 class ProfileSchema(Schema):
-    id = fields.UUID()
+    id = fields.UUID(dump_only=True)
     email = fields.Email(required=True, validate=[validate.Length(max=128)])
-    password = fields.String(load_only=True, required=True, validate=[validate.Length(min=8, max=128)])
+    password = fields.String(load_only=True, required=True, validate=[validate.Length(min=8)])
     created_at = fields.DateTime(dump_only=True)
