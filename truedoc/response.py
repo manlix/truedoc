cd1e3416853d0de6a9b678903f226813c108f61b
manlix <manlix@yandex.ru>
2019-07-27 19:55:08 +0300

    [Error] Change default code to 406 (NOT ACCEPTABLE) for failed requests

--- truedoc/response.py
+++ truedoc/response.py
@@ -1,9 +1,11 @@
+from http import HTTPStatus
+
 from flask import jsonify
 from .constants import STATUS
 
 
-def failure(http_code=400, description=None, **kwargs):
-    """Default return code is 400 if $http_code was not set."""
+def failure(http_code=HTTPStatus.NOT_ACCEPTABLE, description=None, **kwargs):
+    """Default return code is 406 if $http_code was not set."""
     response = {'status': STATUS.ERROR, **kwargs}
 
     if description is not None:
@@ -12,7 +14,7 @@ def failure(http_code=400, description=None, **kwargs):
     return jsonify(response), http_code
 
 
-def success(http_code=200, description=None, **kwargs):
+def success(http_code=HTTPStatus.OK, description=None, **kwargs):
     """Default return code is 200 if $http_code was not set."""
     response = {'status': STATUS.SUCCESS, **kwargs}
 
