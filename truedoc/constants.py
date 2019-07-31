4d04bde992f4ba51fcc2fdb98e78252f35b0df4f
manlix <manlix@yandex.ru>
2019-07-31 16:06:44 +0300

    Add standard sizes to constants module

--- truedoc/constants.py
+++ truedoc/constants.py
@@ -5,3 +5,10 @@ class STATUS:
     """Possible status result of operation in response."""
     ERROR = 'error'
     SUCCESS = 'success'
+
+
+class SIZE:
+    """Standard sizes."""
+    BYTE = 1
+    KILOBYTE = 1024 * BYTE
+    MEGABYTE = 1024 * KILOBYTE
