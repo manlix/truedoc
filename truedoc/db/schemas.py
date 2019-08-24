84407d9d136a09509adafc0c98c21078bbfc8b55
manlix <manlix@yandex.ru>
2019-08-24 23:52:04 +0300

    [Schemas] Rename 'id' to {document,profile}_id

--- truedoc/db/schemas.py
+++ truedoc/db/schemas.py
@@ -4,7 +4,7 @@ from marshmallow import validate
 
 
 class ProfileSchema(Schema):
-    id = fields.UUID(dump_only=True)
+    document_id = fields.UUID(dump_only=True)
     email = fields.Email(required=True, validate=[validate.Length(max=128)])
     password = fields.String(load_only=True, required=True, validate=[validate.Length(min=8)])
     created_at = fields.DateTime(dump_only=True)
@@ -13,7 +13,7 @@ class ProfileSchema(Schema):
 class DocumentSchema(Schema):
     """Document schema. Do NOT use UUID for members with 'load_only=True'
     because SQLAlchemy cannot insert value 'UUID(...)' to database."""
-    id = fields.UUID(dump_only=True)
+    document_id = fields.UUID(dump_only=True)
     profile_id = fields.String(load_only=True, validate=[validate.Length(36)])  # Do NOT use UUID type here. See above.
     title = fields.String(required=True)
     document = fields.Raw(required=True, load_only=True)
