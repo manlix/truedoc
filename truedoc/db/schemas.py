f4c36c3c24fd4c85c276c2956022308ab860c9f2
manlix <manlix@yandex.ru>
2019-08-25 00:15:31 +0300

    [Schema] Update schemas for both 'profile' & 'document'

--- truedoc/db/schemas.py
+++ truedoc/db/schemas.py
@@ -4,20 +4,20 @@ from marshmallow import validate
 
 
 class ProfileSchema(Schema):
-    document_id = fields.UUID(dump_only=True)
+    profile_id = fields.UUID(required=True, dump_only=True)
     email = fields.Email(required=True, validate=[validate.Length(max=128)])
-    password = fields.String(load_only=True, required=True, validate=[validate.Length(min=8)])
-    created_at = fields.DateTime(dump_only=True)
+    password = fields.String(required=True, load_only=True, validate=[validate.Length(min=8)])
+    created_at = fields.DateTime(required=True, dump_only=True)
 
 
 class DocumentSchema(Schema):
     """Document schema. Do NOT use UUID for members with 'load_only=True'
     because SQLAlchemy cannot insert value 'UUID(...)' to database."""
-    document_id = fields.UUID(dump_only=True)
-    profile_id = fields.String(load_only=True, validate=[validate.Length(36)])  # Do NOT use UUID type here. See above.
+    document_id = fields.UUID(required=True, dump_only=True)
+    profile_id = fields.String(required=True, load_only=True, validate=[validate.Length(36)])
     title = fields.String(required=True)
     document = fields.Raw(required=True, load_only=True)
     filename = fields.String(required=True)
     filesize = fields.Integer(required=True, validate=[validate.Range(min=0)])
     digest = fields.String(required=True, validate=[validate.Length(equal=32)])
-    created_at = fields.DateTime(dump_only=True)
+    created_at = fields.DateTime(required=True, dump_only=True)
