0325c2ee3be420da1ab6e102aaf0dcf2e855c02f
manlix <manlix@yandex.ru>
2019-08-24 14:38:11 +0300

    [API-REQUESTS] Add test to 'create document' with size 1KB

--- api-requests/document/document-create-200.py
+++ api-requests/document/document-create-200.py
@@ -2,19 +2,27 @@
 Должен возвращать: 200 (OK)."""
 import requests
 
+from truedoc import constants
+
 endpoint = 'http://truedoc-app.localhost/document/'
-payload = dict(
-    title='Document Simple Title',
-    document='password',
-)
 
-r = requests.post(endpoint, json=payload)
+# Upload file with size=1KB
+payload = {
+    'data': {
+        'title': 'Document Simple Title: 1KB',
+    },
+    'files': [
+        (
+            'document',
+            (
+                'file_1KB.txt',
+                1 * constants.SIZE.KILOBYTE * 'x',
+            )
+        ),
+    ]
 
-assert r.status_code in (
-    200,  # 200 (OK)
-    409,  # 409 (Conflict)
-)
+}
 
+r = requests.post(endpoint, **payload)
 
-
-# Test request: http -v -f POST truedoc-app.localhost/document/  title="document_title" document@~/checm.docx
+assert r.status_code == 200  # OK
