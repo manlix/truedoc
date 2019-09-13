e2a64465c26dc3893947f70a82a9bfef160a085a
manlix <manlix@yandex.ru>
2019-09-13 20:44:20 +0300

    [Tokens] Add decorator for places required valid 'access_token'

---
+++ truedoc/decorators.py
@@ -0,0 +1,33 @@
+"""Decorators."""
+
+import functools
+
+from flask import request
+
+import marshmallow.exceptions
+
+from truedoc import tokens
+import truedoc.exceptions
+from truedoc.db import schemas
+
+
+def require_valid_token(func):
+    """Decorator for sections required valid 'access_token'."""
+
+    @functools.wraps(func)
+    def wrapper(*arg, **kwargs):
+
+        headers = dict(request.headers)  # TODO: think is it wrong to use 'dict()' here or not?
+        authorization_header_schema = schemas.AuthorizationHeaderSchema()
+
+        try:
+            authorization_header_data = authorization_header_schema.load(headers)
+
+        except marshmallow.exceptions.ValidationError:
+            raise truedoc.exceptions.TokenNoValidTokenInHeaderError
+
+        tokens.is_token_valid(authorization_header_data['token'])
+
+        return func(*arg, **kwargs)
+
+    return wrapper
