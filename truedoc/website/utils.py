14513e5cf44fe0678137424ecba10a80904bef83
manlix <manlix@yandex.ru>
2019-09-29 23:50:24 +0300

    Update document processing stage

--- truedoc/website/utils.py
+++ truedoc/website/utils.py
@@ -1,14 +1,18 @@
-import hashlib
+import os
 
 from flask import request
 from werkzeug.datastructures import FileStorage
 from werkzeug.utils import secure_filename
 
+from truedoc.db import schemas
 from truedoc.exceptions import DocumentNoFileInRequest
+
+import truedoc.common
+import truedoc.tasks
 
 
 def uploaded_document():
-    """Helper to make dict for scheme to validate data."""
+    """Helper to make dict for scheme to validate Document processing."""
 
     # See 'Uploading files': https://flask.palletsprojects.com/en/1.1.x/patterns/fileuploads/
 
@@ -16,15 +20,41 @@ def uploaded_document():
         raise DocumentNoFileInRequest
 
     # TODO: should saves file to object storage.
-    document_in_memory = request.files['document'].read()
+    # document_in_memory = request.files['document'].read()
 
-    document = {
-        'profile_id': request.form.get('profile_id', None),
-        'title': request.form.get('title', None),
-        'document': document_in_memory,
-        'filename': secure_filename(request.files['document'].filename),
-        'digest': hashlib.md5(document_in_memory).hexdigest(),
-        'filesize': len(document_in_memory),
+    profile_id = request.form.get('profile_id', None)  # TODO: read profile_id from JWT
+    document_id = truedoc.common.uuid4()
+    title = request.form.get('title', None)  # TODO: edit schema to replace empty string ('') to None
+
+    filename = secure_filename(request.files['document'].filename)  # TODO: handle None of the filename
+
+    request.files['document'].save(os.path.join('/upload', document_id))
+
+    just_uploaded = {
+        'document_id': document_id,
+        'profile_id': profile_id,
+        'title': title,
+        'filename': filename,
     }
 
-    return document
+    schema_DocumentWorkerProcessing = schemas.DocumentWorkerProcessingSchema()
+    data_DocumentWorkerProcessing = schema_DocumentWorkerProcessing.load(just_uploaded)
+
+    # See: https://docs.celeryproject.org/en/master/reference/celery.app.task.html#celery.app.task.Task.apply_async
+    task = truedoc.tasks.process_document.apply_async(
+        args=(data_DocumentWorkerProcessing,),
+        task_id=document_id,
+    )
+
+    schema_DocumentProcessing = schemas.DocumentProcessingSchema()
+
+    data_tmp = schema_DocumentWorkerProcessing.dump(data_DocumentWorkerProcessing)
+    data_tmp.update(
+        {
+            'state': task.state,
+        }
+    )
+
+    data_DocumentProcessing = schema_DocumentProcessing.load(data_tmp)
+
+    return schema_DocumentProcessing.dump(data_DocumentProcessing)
