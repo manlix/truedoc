05bad291123b9f82343d40251fd7919e703452fe
manlix <manlix@yandex.ru>
2020-05-10 10:32:49 +0300

    Drop 'bookmytime' entity (close #62, close #32)

--- truedoc/website/app.py
+++ truedoc/website/app.py
@@ -18,7 +18,6 @@ from truedoc.exceptions import TruedocError
 
 from truedoc.response import failure
 from truedoc.website.blueprints import auth
-from truedoc.website.blueprints import bookmytime
 from truedoc.website.blueprints import document
 from truedoc.website.blueprints import error
 from truedoc.website.blueprints import profile
@@ -45,7 +44,6 @@ app.register_blueprint(error.bp)
 app.register_blueprint(profile.bp, url_prefix='/profile')
 app.register_blueprint(document.bp, url_prefix='/document')
 app.register_blueprint(auth.bp, url_prefix='/auth')
-app.register_blueprint(bookmytime.bp, url_prefix='/bookmytime')
 
 
 # TODO: see error handling manual
